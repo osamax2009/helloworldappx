c71450342fbad2b377e0f87c5043150ab1f3340a
MSCosamaShallal <osamafalah85@yahoo.com>
2023-01-07 10:39:41 +0300

    add Task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
 import Header  from './components/Header'
-
+import Tasks  from './components/Tasks';
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks , setTasks] = useState([
+    {
+        id:1,
+        text:'go to the doctor',
+        day: '5th of fab at 2:00 am',
+        remainder: true 
+    },
+    {
+        id:2,
+        text:'go to the lecture',
+        day: '10th of jun at 3:00 am',
+        remainder: true 
+    },
+    {
+        id:3,
+        text:'go to the dentist',
+        day: '5th of jan at 5:00 pm',
+        remainder: false 
+    },
+    
+]);
+
+
   return (
     <div className="container">
       <Header title='To Do List' />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
